b857e6f89e94dd890d99f0605011e54868aef6e5
morriswa <william@morriswa.org>
2024-03-26 15:14:14 -0500

    # change creds location in local storage

--- src/app/service/credential.service.ts
+++ src/app/service/credential.service.ts
@@ -4,38 +4,44 @@ import {BehaviorSubject} from "rxjs";
 @Injectable()
 export class CredentialService {
 
+  private static TOKEN_STORE = "org.morriswa.salon.credentials.token";
+  private static UNAME_STORE = "org.morriswa.salon.credentials.username";
+
   private _token$:BehaviorSubject<string|undefined> = new BehaviorSubject<string | undefined>(undefined);
   private _username$:BehaviorSubject<string|undefined> = new BehaviorSubject<string | undefined>(undefined);
 
   constructor() {
-    let token= localStorage.getItem("org.morriswa.eecs447.credentials.token");
+    let token= localStorage.getItem(CredentialService.TOKEN_STORE);
     if (token) this._token$.next(token);
-    let username = localStorage.getItem("org.morriswa.eecs447.credentials.username");
+    let username = localStorage.getItem(CredentialService.UNAME_STORE);
     if (username) this._username$.next(username);
   }
 
   public createCredentials(username: string, password:string) {
-    let unencodedToken = `${username}:${password}`;
-    let encodedToken = btoa(unencodedToken);
-    this._username$.next(username);
+    const rawToken = `${username}:${password}`;
+    const encodedToken = btoa(rawToken);
+
     this._token$.next(`Basic ${encodedToken}`);
-    localStorage.setItem("org.morriswa.eecs447.credentials.username", this._username$.getValue()!);
-    localStorage.setItem("org.morriswa.eecs447.credentials.token", this._token$.getValue()!);
+    this._username$.next(username);
+
+    localStorage.setItem(CredentialService.TOKEN_STORE, this._token$.value!);
+    localStorage.setItem(CredentialService.UNAME_STORE, this._username$.value!);
   }
 
   public deleteStoredCredentials() {
     this._token$.next(undefined);
-    localStorage.removeItem("org.morriswa.eecs447.credentials.token");
+    localStorage.removeItem(CredentialService.TOKEN_STORE);
+
     this._username$.next(undefined);
-    localStorage.removeItem("org.morriswa.eecs447.credentials.username");
+    localStorage.removeItem(CredentialService.UNAME_STORE);
   }
 
   public get ready(): boolean {
-    return !!(this._token$.getValue() && this._username$.getValue());
+    return !!(this._token$.value && this._username$.value);
   }
 
   public get token(): string {
-    const token = this._token$.getValue();
+    const token = this._token$.value;
     if (!token) throw new Error("\"Credentials service has not been initialized! Please log in\"")
     return token;
   }
