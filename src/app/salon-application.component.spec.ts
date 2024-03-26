7bbee2ea7506bbdd745d725460bb1a393111a7c4
morriswa <william@morriswa.org>
2024-03-26 15:41:23 -0500

    - old components n specs

--- src/app/salon-application.component.spec.ts
+++ src/app/salon-application.component.spec.ts
@@ -1,12 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 import { SalonApplication } from './salon-application.component';
+import {LoginService} from "./service/login.service";
+import {CredentialService} from "./service/credential.service";
+import {SalonClient} from "./service/salon-client.service";
+import {provideHttpClientTesting} from "@angular/common/http/testing";
+import {HttpClient, provideHttpClient} from "@angular/common/http";
 
-describe('AppComponent', () => {
+describe('SalonApplication', () => {
+
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
     imports: [
         SalonApplication
     ],
+    providers: [
+      LoginService,
+      CredentialService,
+      SalonClient,
+      provideHttpClientTesting(),
+      provideHttpClient()
+    ]
 }).compileComponents();
   });
 
@@ -19,6 +33,6 @@ describe('AppComponent', () => {
   it('should call login init', () => {
     const fixture = TestBed.createComponent(SalonApplication);
     const app = fixture.componentInstance;
-    expect(app.loginService.init).toHaveBeenCalledOnceWith();
+    expect(app.loginService).toHaveBeenCalledOnceWith();
   });
 });
