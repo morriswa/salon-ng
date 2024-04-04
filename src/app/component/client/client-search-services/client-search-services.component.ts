4787289e637632f75a48478efc381af23a315ff9
morriswa <william@morriswa.org>
2024-04-03 20:11:46 -0500

    # tabs for service category

--- src/app/component/client/client-search-services/client-search-services.component.ts
+++ src/app/component/client/client-search-services/client-search-services.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {FormControl, ReactiveFormsModule} from "@angular/forms";
 import {SalonClient} from "../../../service/salon-client.service";
-import {BehaviorSubject} from "rxjs";
+import {BehaviorSubject, of, switchMap, tap} from "rxjs";
 import {CommonModule} from "@angular/common";
 import {ValidatorFactory} from "../../../validator-factory";
 import {PageService} from "../../../service/page.service";
@@ -18,15 +18,51 @@ import {PageService} from "../../../service/page.service";
 })
 export class ClientSearchServicesComponent {
 
-  availableServiceSearchResults$: BehaviorSubject<any[]> = new BehaviorSubject<any[]>([]);
+  availableServiceSearchResults$: BehaviorSubject<any[]>
+    = new BehaviorSubject<any[]>([]);
+
+  currentPage$: BehaviorSubject<'hair'|'nail'|'other'>
+    = new BehaviorSubject<'hair'|'nail'|'other'>('hair');
+
+  loading$: BehaviorSubject<boolean>
+    = new BehaviorSubject<boolean>(true);
+
 
   searchServiceForm: FormControl = ValidatorFactory.getGenericForm();
 
 
-  constructor(private salonClient: SalonClient, public page: PageService) { }
+  constructor(private salonClient: SalonClient, public page: PageService) {
+    this.salonClient.searchAvailableServices(this.currentPage$.value)
+      .subscribe({
+        next: res=>{
+          this.availableServiceSearchResults$.next(res);
+        }
+      });
+
+    this.currentPage$.asObservable()
+      .pipe(
+        tap(()=>this.loading$.next(true)),
+        switchMap((res)=>{
+          switch (res) {
+            case 'hair':
+              return this.salonClient.searchAvailableServices('hair')
+                .pipe(tap(()=>this.loading$.next(false)));
+            case 'nail':
+              return this.salonClient.searchAvailableServices('nail')
+                .pipe(tap(()=>this.loading$.next(false)));
+            default:
+              return of([]).pipe(tap(()=>this.loading$.next(false)));
+          }
+      }))
+      .subscribe({
+        next: res=>{
+          this.availableServiceSearchResults$.next(res);
+        }
+      });
+  }
 
 
-  searchAvailableServices($event: KeyboardEvent) {
+  searchAvailableServices() {
 
     let searchText = this.searchServiceForm.value;
     this.salonClient.searchAvailableServices(searchText)
